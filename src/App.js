20fd31664852bfd553d64fcb8fcf7504cc479df6
sina42048 <sinafat76@gmail.com>
2021-09-24 11:25:27 +0330

    modified and deleted some files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
+import Login from './components/Login';
+import Register from './components/Register';
+import Profile from './components/Profile';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <div className="container center gap">
+            <Login />
+          </div>
+        </Route>
+        <Route path="/register">
+          <div className="container center gap">
+            <Register />
+          </div>
+        </Route>
+        <Route path="/profile">
+          <div className="container center gap">
+            <Profile />
+          </div>
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
